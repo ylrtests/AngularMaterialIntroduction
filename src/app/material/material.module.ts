91014d9651b6b1b4e4cd5ea3fb2d20cbf07d5ec2
Pipo <pipo@MacBook-Air-de-Apple.local>
2020-07-03 21:28:44 -0500

    Buttons Toggle Lesson

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatButtonToggleModule
 ]
 
 
 @NgModule({
   exports: [
-    MaterialComponents
+    MaterialComponents,
   ],
   imports: [
     MaterialComponents
